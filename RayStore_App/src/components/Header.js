28b82a5d675cb7dccdf2b42cedc4aa29464eb3ab
Raywill <107601109+RayStore@users.noreply.github.com>
2022-11-22 19:01:46 +0300

    added icons

--- RayStore_App/src/components/Header.js
+++ RayStore_App/src/components/Header.js
@@ -1,20 +1,22 @@
 import React from 'react';
-  
+
 export default function Header() {
     return (
         <header className="header">
             <div className="header__logotype">
-                    
-                    <span className="logo">Ray Autoparts</span>
-                    <ul className="navigate">
-                        <li>О нас</li>
-                        <li>Новости</li>
-                        <li>Контакты</li>
-                        <li>Регистрация</li>
-                        <li>Вход</li>
-                    </ul>
-                </div>
-                <div className="presentation"></div>
+
+                <span className="logo">Ray Autoparts
+                    <img src="./public/logo_2" alt="logotype"></img>
+                </span>
+                <ul className="navigate">
+                    <li>О нас</li>
+                    <li>Новости</li>
+                    <li>Контакты</li>
+                    <li>Регистрация</li>
+                    <li>Вход</li>
+                </ul>
+            </div>
+            <div className="presentation"></div>
         </header>
     )
 }
